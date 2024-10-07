5cb11ff2884657369eb97fd510ea45481e1807c8
abhishekbaraik333 <abhitamangme@gmail.com>
2024-10-08 01:56:40 +0530

    team section done!

--- script.js
+++ script.js
@@ -262,3 +262,21 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 });
+
+// Inspiration -  Frontend Mentor
+// https://www.frontendmentor.io/challenges/myteam-multipage-website-mxlEauvW
+const cardControllers = document.querySelectorAll("[data-card-controller]");
+
+cardControllers.forEach(controller => {
+  controller.addEventListener("click", (e) => {
+    const card = e.currentTarget.parentElement.parentElement;
+    const isVisible = card.dataset.visible;
+
+    if (isVisible === "false") {
+      card.setAttribute("data-visible", true);
+    } else {
+      card.setAttribute("data-visible", false);
+    }
+  })
+})
+
