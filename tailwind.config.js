7a9f8f044dffb148410b3b362353b9c14f4f52f5
abhishekbaraik333 <abhitamangme@gmail.com>
2024-11-11 17:33:01 +0530

    okk done tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./**/*.html", "./pages/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#0C3C60',
+        secondary: '#EBE87A',
+      }
+    },
+  },
+  plugins: [],
+};
